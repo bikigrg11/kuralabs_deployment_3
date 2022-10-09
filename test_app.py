3e89838d0feeb0a358eb5c250d54905608ea8f21
Biki Gurung <bikigurung@gmail.com>
2022-10-09 01:45:27 -0400

    unit test changed

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 from application import app, greet
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff "
+# def test_quick():
+#   a = "jeff"
+#   greeting = greet(a)
+#   assert greeting == "Hi jeff "
 
 # def test_home_page():
 #     response = app.test_client().get('/')
